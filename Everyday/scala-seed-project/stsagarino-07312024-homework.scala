54cb9d82efd4ebce84d6cee4aadd5b16075772bc
Stephen T. Sagarino Jr <stsagarino@vauldex.com>
2024-08-01 09:15:24 +0800

    New Questions

--- Everyday/scala-seed-project/stsagarino-07312024-homework.scala
+++ Everyday/scala-seed-project/stsagarino-07312024-homework.scala
@@ -1,32 +1,34 @@
+import Printer.{printerln as println, printer as print}
+
 // Functor is with a map method that applies a given function to a value it contains, resulting in a new Functor of the same type class.
 
 // Implement a type class named Functor that includes a map method that applies a given function to a value it contains, resulting in a new Functor of the same type class, and instances of the type class for option and list.
 
 trait Functor[T[_]]:
 	def map[A,B](value:T[A])(f:A=>B):T[B]
+
 object Functor:
 	given Functor[Option] with
-		def map[A,B](value:Option[A])(f:A=>B):Option[B] =
+		def map[A,B](value:Option[A])(f:A=>B):Option[B]=
 			value match {
 				case None => None
-				case Some(a) => Some(f(a))
+				case Some(a) => Some(f(a)) 
 			}
 	given Functor[List] with
 		def map[A,B](value:List[A])(f:A=>B):List[B]=
 			value match {
 				case Nil => Nil
-				case list:List[A] => list.map(f)
+				case list:List[A] => list.map(f) 
 			}
+// extension[T](opt:Option[T])(using func:Functor[Option])
+// 	def functorForOption[B](f:T=>B):Option[B]= 
+// 		func.map(opt)(f)
 
-extension[T](opt:Option[T])(using func:Functor[Option])
-	def functorForOption[B](f:T=>B):Option[B]= 
-		func.map(opt)(f)
+extension[T,A[_]](value:A[T])(using func:Functor[A])
+	def functor[B](f:T=>B):A[B]=
+		func.map(value)(f)
 
-
-extension[T](opt:List[T])(using func:Functor[List])
-	def functorForList[B](f:T=>B):List[B]= 
-		func.map(opt)(f)
-
-@main def stsagarino =
-	println(Some("a").functorForOption(x=>x+"b"))
-	println(List(1,2,3,4).functorForList(x=>x+1))
+@main def stsagarino = 
+	val meow:Option[String] = None
+	println(meow.functor(x=>x+"B"))
+	println(List(1,2,3,4,5).functor(x=>x+1))
